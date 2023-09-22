04834e5d3151d64ab13c825534b732d8eb9c89e9
s3836565 <62092164+s3836565@users.noreply.github.com>
2023-09-22 20:16:46 +0700

    change output to booolean

--- PortMySelft/src/Weight/Weights.java
+++ PortMySelft/src/Weight/Weights.java
@@ -6,14 +6,23 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 public class Weights {
 
     public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
         Map<String, Integer> containerData = readContainerData("PortMySelft/src/Data/Container.txt");
         Map<String, Integer> portData = readPortData("PortMySelft/src/Data/Port.txt");
         Map<String, String[]> vehicleData = readVehicleData("PortMySelft/src/Data/Vehicle.txt");
 
-        checkWeightLimit("v-001", "p-003", containerData, portData, vehicleData);
+        System.out.println("Welcome to the Port Management System");
+        System.out.print("Enter the vehicle ID: ");
+        String vehicleId = scanner.nextLine();
+        System.out.print("Enter the port ID: ");
+        String portId = scanner.nextLine();
+// output de lay di//
+        boolean isWithinLimit = checkWeightLimit(vehicleId, portId, containerData, portData, vehicleData);
+        System.out.println("Is within limit: " + (isWithinLimit ? "Yes" : "No"));
     }
 
     public static Map<String, Integer> readContainerData(String fileName) throws IOException {
@@ -60,8 +69,39 @@ public class Weights {
         }
         return data;
     }
+// output as text
+//    public static void checkWeightLimit(String vehicleId, String portId, Map<String, Integer> containerData, Map<String, Integer> portData, Map<String, String[]> vehicleData) {
+//        int totalWeight = 0;
+//        String[] containers = vehicleData.get(vehicleId);
+//
+//        if (containers != null) {
+//            for (String container : containers) {
+//                Integer containerWeight = containerData.get(container);
+//                if (containerWeight != null) {
+//                    totalWeight += containerWeight;
+//                } else {
+//                    System.out.println("Container " + container + " not found in containerData");
+//                }
+//            }
+//        } else {
+//            System.out.println("Vehicle " + vehicleId + " not found in vehicleData");
+//        }
+//
+//        Integer portLimit = portData.get(portId);
+//        if (portLimit != null) {
+//            if (totalWeight > portLimit) {
+//                System.out.println("The total weight of containers for vehicle " + vehicleId + " exceeds the port limit of " + portId);
+//            } else {
+//                System.out.println("The total weight of containers for vehicle " + vehicleId + " is within the port limit of " + portId);
+//            }
+//        } else {
+//            System.out.println("Port " + portId + " not found in portData");
+//        }
+//    }
+//}
 
-    public static void checkWeightLimit(String vehicleId, String portId, Map<String, Integer> containerData, Map<String, Integer> portData, Map<String, String[]> vehicleData) {
+    // output as boolen
+    public static boolean checkWeightLimit(String vehicleId, String portId, Map<String, Integer> containerData, Map<String, Integer> portData, Map<String, String[]> vehicleData) {
         int totalWeight = 0;
         String[] containers = vehicleData.get(vehicleId);
 
@@ -70,23 +110,15 @@ public class Weights {
                 Integer containerWeight = containerData.get(container);
                 if (containerWeight != null) {
                     totalWeight += containerWeight;
-                } else {
-                    System.out.println("Container " + container + " not found in containerData");
                 }
             }
-        } else {
-            System.out.println("Vehicle " + vehicleId + " not found in vehicleData");
         }
 
         Integer portLimit = portData.get(portId);
         if (portLimit != null) {
-            if (totalWeight > portLimit) {
-                System.out.println("The total weight of containers for vehicle " + vehicleId + " exceeds the port limit of " + portId);
-            } else {
-                System.out.println("The total weight of containers for vehicle " + vehicleId + " is within the port limit of " + portId);
-            }
-        } else {
-            System.out.println("Port " + portId + " not found in portData");
+            return totalWeight <= portLimit;
         }
+        return false;
     }
+
 }
